286134fe26cb961fec1eff9eb6bb544e54ca0285
Eric S. Tellez <donsadit@gmail.com>
2022-07-20 13:41:42 -0500

    uso de L2 en lugar de dcos en 07

--- run-pandoc.sh
+++
@@ -1,9 +0,0 @@
-./pandoc index.md -c pandoc.css -s --katex  -o index.html
-
-for index in U?/index.md
-do
-    d=$(dirname $index)
-    cd $d
-    ../pandoc index.md -c ../pandoc.css -s --mathjax -o index.html
-    cd ..
-done
